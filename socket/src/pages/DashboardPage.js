2e1a7a7183e13cb402dd4590c4006f432b77f7d6
sonchaegeon <thscorjs1002@naver.com>
2021-01-16 00:42:09 +0900

    UPDATE; get chatrooms api

--- socket/src/pages/DashboardPage.js
+++ socket/src/pages/DashboardPage.js
@@ -1,6 +1,27 @@
 import React from 'react';
+import axios from 'axios';
+import { Link } from "react-router-dom"
 
 const DashboardPage = () => {
+    const [chatrooms, setChatrooms] = React.useState([]);
+
+    const getChatrooms = () => {
+        axios.get('http://localhost:3000/chatroom', {
+            headers: {
+                Authorization: "Bearer " + localStorage.getItem('Token'),
+            },
+        }).then(response => {
+            setChatrooms(response.data);
+        }).catch(err => {
+            setTimeout(getChatrooms, 3000);
+        });
+    }
+
+    React.useEffect(() => {
+        getChatrooms()
+        // exlint-disable-next-line
+    }, []);
+
     return (
         <div className="card">
             <div className="cardHeader">Chatrooms</div>
@@ -11,10 +32,14 @@ const DashboardPage = () => {
                 </div>
                 <button>Create Chatroom</button>
                 <div className="chatrooms">
-                    <div className="chatroom">
-                        <div>Happy Newbie</div>
-                        <div className="join">Join</div>
-                    </div>
+                    {chatrooms.map((chatroom) => (
+                        <div key={chatroom._id} className="chatroom">
+                            <div>{chatroom.name}</div>
+                            <Link to={"/chatroom/" + chatroom._id}>
+                                <div className="join">Join</div>
+                            </Link>
+                        </div>
+                    ))}
                 </div>
             </div>
         </div>
