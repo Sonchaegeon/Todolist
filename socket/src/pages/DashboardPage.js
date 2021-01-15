1745821c0ab36405dd44e14ccccbc2bbaf07adf7
sonchaegeon <thscorjs1002@naver.com>
2021-01-15 21:34:49 +0900

    UPDATE; dashboard page form

--- socket/src/pages/DashboardPage.js
+++ socket/src/pages/DashboardPage.js
@@ -2,8 +2,21 @@ import React from 'react';
 
 const DashboardPage = () => {
     return (
-        <div>
-            Dashboard Page
+        <div className="card">
+            <div className="cardHeader">Chatrooms</div>
+            <div className="cardBody">
+                <div className="inputGroup">
+                    <label htmlFor="chatroomName">Chatroom Name</label>
+                    <input type="text" name="chatroomName" id="chatroomName" placeholder="Chatroom Name"></input>
+                </div>
+                <button>Create Chatroom</button>
+                <div className="chatrooms">
+                    <div className="chatroom">
+                        <div>Happy Newbie</div>
+                        <div className="join">Join</div>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 };
