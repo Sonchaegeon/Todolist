4864ed027d15e0ea06a712433de75e9ec90f7501
sonchaegeon <thscorjs1002@naver.com>
2021-01-15 21:34:40 +0900

    UPDATE; login page form

--- socket/src/pages/LoginPage.js
+++ socket/src/pages/LoginPage.js
@@ -2,7 +2,20 @@ import React from 'react';
 
 const LoginPage = () => {
     return (
-    <div>Login page</div>
+        <div className="card">
+            <div className="cardHeader">Login</div>
+            <div className="cardBody">
+                <div className="inputGroup">
+                    <label htmlFor="email">Email</label>
+                    <input type="email" name="email" id="email" placeholder="abc@gmail.com"></input>
+                </div>
+                <div className="inputGroup">
+                    <label htmlFor="password">Password</label>
+                    <input type="password" name="password" id="password" placeholder="Your Password"></input>
+                </div>
+                <button>Login</button>
+            </div>
+        </div>
     );
 }
 
