177832d6fca76c1053fc49e3ce1f48b51734fe83
sonchaegeon <thscorjs1002@naver.com>
2021-01-15 21:34:30 +0900

    UPDATE; register page form

--- socket/src/pages/RegisterPage.js
+++ socket/src/pages/RegisterPage.js
@@ -2,8 +2,23 @@ import React from 'react';
 
 const RegisterPage = () => {
     return (
-        <div>
-            Register Page
+        <div className="card">
+            <div className="cardHeader">Registeration</div>
+            <div className="cardBody">
+                <div className="inputGroup">
+                    <label htmlFor="name">Name</label>
+                    <input type="text" name="name" id="name" placeholder="Sonchaegeon"></input>
+                </div>
+                <div className="inputGroup">
+                    <label htmlFor="email">email</label>
+                    <input type="email" name="email" id="email" placeholder="abc@gmail.com"></input>
+                </div>
+                <div className="inputGroup">
+                    <label htmlFor="password">Password</label>
+                    <input type="password" name="password" id="password" placeholder="Your Password"></input>
+                </div>
+                <button>Register</button>
+            </div>
         </div>
     );
 };
