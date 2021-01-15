772ecd34402e455a0eda44355bdc71112911f8f1
sonchaegeon <thscorjs1002@naver.com>
2021-01-15 22:54:35 +0900

    UPDATE; login api

--- socket/src/pages/LoginPage.js
+++ socket/src/pages/LoginPage.js
@@ -1,19 +1,40 @@
+import axios from 'axios';
 import React from 'react';
+import makeToast from '../Toaster';
 
-const LoginPage = () => {
+const LoginPage = (props) => {
+    const emailRef = React.createRef();
+    const passwordRef = React.createRef();
+
+    const loginUser = () => {
+        const email = emailRef.current.value;
+        const password = passwordRef.current.value;
+
+        axios.post("http://localhost:3000/user/login", {
+            email,
+            password
+        }).then(response => {
+            makeToast("success", response.data.message);
+            localStorage.setItem('Token');
+            props.history.push('/dashboard');
+        }).catch(err => {
+            makeToast("error", err.response.data.message);
+        });
+    }
+
     return (
         <div className="card">
             <div className="cardHeader">Login</div>
             <div className="cardBody">
                 <div className="inputGroup">
                     <label htmlFor="email">Email</label>
-                    <input type="email" name="email" id="email" placeholder="abc@gmail.com"></input>
+                    <input type="email" name="email" id="email" placeholder="abc@gmail.com" ref={emailRef}></input>
                 </div>
                 <div className="inputGroup">
                     <label htmlFor="password">Password</label>
-                    <input type="password" name="password" id="password" placeholder="Your Password"></input>
+                    <input type="password" name="password" id="password" placeholder="Your Password" ref={passwordRef}></input>
                 </div>
-                <button>Login</button>
+                <button onClick={loginUser}>Login</button>
             </div>
         </div>
     );
