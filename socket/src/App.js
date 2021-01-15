f03cb048f329eb2908b2f70645b2d37e9b644102
sonchaegeon <thscorjs1002@naver.com>
2021-01-16 00:42:24 +0900

    UPDATE; route chatroom

--- socket/src/App.js
+++ socket/src/App.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { BrowserRouter, Route, Switch } from "react-router-dom";
+import ChatroomPage from "./pages/ChatroomPage";
 import DashboardPage from "./pages/DashboardPage";
 import IndexPage from "./pages/IndexPage";
 import LoginPage from "./pages/LoginPage";
@@ -12,6 +13,7 @@ function App() {
       <Route path="/login" component={LoginPage} exact></Route>
       <Route path="/register" component={RegisterPage} exact></Route>
       <Route path="/dashboard" component={DashboardPage} exact></Route>
+      <Route path="/chatroom/:id" component={ChatroomPage} exact></Route>
     </Switch>
   </BrowserRouter>;
 }
