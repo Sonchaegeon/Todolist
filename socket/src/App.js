68e76f3a1cf8487f916cb02451bd350b9171df94
sonchaegeon <thscorjs1002@naver.com>
2021-01-15 23:05:54 +0900

    UPDATE; route exact

--- socket/src/App.js
+++ socket/src/App.js
@@ -1,15 +1,17 @@
 import React from "react";
 import { BrowserRouter, Route, Switch } from "react-router-dom";
 import DashboardPage from "./pages/DashboardPage";
+import IndexPage from "./pages/IndexPage";
 import LoginPage from "./pages/LoginPage";
 import RegisterPage from "./pages/RegisterPage";
 
 function App() {
   return <BrowserRouter>
     <Switch>
-      <Route path="/login" component={LoginPage}></Route>
-      <Route path="/register" component={RegisterPage}></Route>
-      <Route path="/dashboard" component={DashboardPage}></Route>
+      <Route path="/" component={IndexPage} exact></Route>
+      <Route path="/login" component={LoginPage} exact></Route>
+      <Route path="/register" component={RegisterPage} exact></Route>
+      <Route path="/dashboard" component={DashboardPage} exact></Route>
     </Switch>
   </BrowserRouter>;
 }
