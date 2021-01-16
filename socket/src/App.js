289cdb9394cbb4c2a4c3a778db0b026a03f76e22
sonchaegeon <thscorjs1002@naver.com>
2021-01-16 15:20:29 +0900

    UPDATE; router

--- socket/src/App.js
+++ socket/src/App.js
@@ -1,11 +1,11 @@
 import React from "react";
-import io from "socket.io-client";
-import { BrowserRouter, Route, Switch } from "react-router-dom";
-import ChatroomPage from "./pages/ChatroomPage";
-import DashboardPage from "./pages/DashboardPage";
-import IndexPage from "./pages/IndexPage";
+import { BrowserRouter, Switch, Route } from "react-router-dom";
 import LoginPage from "./pages/LoginPage";
 import RegisterPage from "./pages/RegisterPage";
+import DashboardPage from "./pages/DashboardPage";
+import IndexPage from "./pages/IndexPage";
+import ChatroomPage from "./pages/ChatroomPage";
+import io from "socket.io-client";
 import makeToast from "./Toaster";
 
 function App() {
@@ -13,26 +13,26 @@ function App() {
 
   const setupSocket = () => {
     const token = localStorage.getItem("Token");
-    if(token.length > 0 && !socket) {
+    if (token && !socket) {
       const newSocket = io("http://localhost:3000", {
         query: {
-            token: localStorage.getItem('Token')
-        }
+          token: localStorage.getItem("Token"),
+        },
       });
 
       newSocket.on("disconnect", () => {
         setSocket(null);
         setTimeout(setupSocket, 3000);
-        makeToast('error', "Socket Disconnected!");
+        makeToast("error", "Socket Disconnected!");
       });
 
       newSocket.on("connect", () => {
-        makeToast('success', "Socket Connected!");
+        makeToast("success", "Socket Connected!");
       });
 
       setSocket(newSocket);
     }
-  }
+  };
 
   React.useEffect(() => {
     setupSocket();
@@ -40,15 +40,28 @@ function App() {
   }, []);
 
   return (
-  <BrowserRouter>
-    <Switch>
-      <Route path="/" component={IndexPage} exact></Route>
-      <Route path="/login" render={()=><LoginPage setupSocket={setupSocket}/>} exact></Route>
-      <Route path="/register" component={RegisterPage} exact></Route>
-      <Route path="/dashboard" rander={()=><DashboardPage socket={socket}/>} exact></Route>
-      <Route path="/chatroom/:id" rander={()=><ChatroomPage socket={socket}/>} exact></Route>
-    </Switch>
-  </BrowserRouter>);
+    <BrowserRouter>
+      <Switch>
+        <Route path="/" component={IndexPage} exact />
+        <Route
+          path="/login"
+          render={() => <LoginPage setupSocket={setupSocket} />}
+          exact
+        />
+        <Route path="/register" component={RegisterPage} exact />
+        <Route
+          path="/dashboard"
+          render={() => <DashboardPage socket={socket} />}
+          exact
+        />
+        <Route
+          path="/chatroom/:id"
+          render={() => <ChatroomPage socket={socket} />}
+          exact
+        />
+      </Switch>
+    </BrowserRouter>
+  );
 }
 
 export default App;
