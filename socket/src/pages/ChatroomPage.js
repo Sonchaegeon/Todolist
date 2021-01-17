9dc930407fc9b24c2d827f154353c132cc8f489b
sonchaegeon <thscorjs1002@naver.com>
2021-01-17 14:31:07 +0900

    UPDATE; socket emit chat room message, map messages

--- socket/src/pages/ChatroomPage.js
+++ socket/src/pages/ChatroomPage.js
@@ -3,22 +3,56 @@ import { withRouter } from 'react-router-dom';
 
 const ChatroomPage = ({match, socket}) => {
     const chatroomId = match.params.id;
+    const [messages, setMessages] = React.useState([]);
+    const messageRef = React.useRef();
+
+    const sendMessage = () => {
+        if(socket) {
+            socket.emit("chatroomMessage", {
+                chatroomId,
+                message: messageRef.current.value,
+            });
+        }
+    }
+
+    React.useEffect(() => {
+        socket.emit("joinRoom", {
+            chatroomId,
+        });
+
+        socket.on("newMessage", (message) => {
+            setMessages([...messages, message]);
+        })
+
+        return () => {
+            //Component Unmount
+            if(socket){
+                socket.emit("leaveRoom", {
+                    chatroomId
+                });
+            }
+        }
+
+        //eslint-disable-next-line
+    }, [])
 
     return (
         <div className="chatroomPage">
             <div className="chatroomSection">
                 <div className="cardHeader">Chatroom Name</div>
                 <div className="chatroomContent">
-                    <div className="message">
-                        <span className="otherMessage">Son:</span> qweqwe
-                    </div>
+                    {messages.map(message => {
+                        <div key={chatroomId} className="message">
+                            <span className="otherMessage">{message.name}</span>{message.message}
+                        </div>
+                    })}
                 </div>
                 <div className="chatroomActions">
                     <div>
-                        <input type="text" name="message" placeholder="Say Something!"/>
+                        <input type="text" name="message" placeholder="Say Something!" ref={messageRef}/>
                     </div>
                     <div>
-                        <button className="join">Send</button>
+                        <button className="join" onClick={sendMessage}>Send</button>
                     </div>
                 </div>
             </div>
