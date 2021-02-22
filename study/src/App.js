8bda88c8c9d3766615d0bfcfd2e33c0a607964dc
sonchaegeon <thscorjs1002@naver.com>
2021-02-22 17:17:06 +0900

    CREATE input sample

--- study/src/App.js
+++ study/src/App.js
@@ -1,12 +1,10 @@
 import React from 'react';
-import Hello from './Hello';
+import InputSample from './InputSample';
 
 function App() {
   return (
     <div>
-      <Hello />
-      <Hello />
-      <Hello />
+      <InputSample />
     </div>
   );
 }
