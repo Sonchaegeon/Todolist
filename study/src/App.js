639b64975e7c61a9650eb54376f2014e389920be
sonchaegeon <thscorjs1002@naver.com>
2021-03-07 18:56:03 +0900

    UPDATE user list

--- study/src/App.js
+++ study/src/App.js
@@ -1,12 +1,31 @@
-import React from 'react';
+import React, { useRef } from 'react';
 import UserList from './UserList';
 
 function App() {
-  return (
-    <div>
-      <UserList />
-    </div>
-  );
+  const users = [
+    {
+      id: 1,
+      username: '손채건',
+      email: 'thscorjs1002@naver.com',
+    },
+    {
+      id: 2,
+      username: '조호원',
+      email: 'howon@naver.com',
+    },
+    {
+      id: 3,
+      username: '정지원',
+      email: 'jiwon@naver.com',
+    },
+  ];
+
+  const nextId = useRef(4);
+  const onCreate = () => {
+    nextId.current += 1;
+  };
+
+  return <UserList users={users} />;
 }
 
 export default App;
