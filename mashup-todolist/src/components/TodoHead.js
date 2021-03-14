2c09c9df3fa5ac02aca59bb900637f98c5f6fda4
sonchaegeon <thscorjs1002@naver.com>
2021-03-15 08:27:24 +0900

    todo list

--- mashup-todolist/src/components/TodoHead.js
+++ mashup-todolist/src/components/TodoHead.js
@@ -29,7 +29,7 @@ function TodoHead() {
   return (
     <TodoHeadBlock>
       <h1>2021년 3월 14일</h1>
-      <div className="day">수요일</div>
+      <div className="day">일요일</div>
       <div className="tasks-left">할 일 2개 남음</div>
     </TodoHeadBlock>
   );
