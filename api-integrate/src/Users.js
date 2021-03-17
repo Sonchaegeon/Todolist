6021b7d906f88fd581e4eaba3890320702a880c9
sonchaegeon <thscorjs1002@naver.com>
2021-03-17 19:57:57 +0900

    api-integrate finish

--- api-integrate/src/Users.js
+++ api-integrate/src/Users.js
@@ -1,19 +1,16 @@
 import React, { useState } from 'react';
-import axios from 'axios';
-import useAsync from './useAsync';
 import User from './User';
-
-async function getUsers() {
-  const response = await axios.get(
-    'https://jsonplaceholder.typicode.com/users'
-  );
-  return response.data;
-}
+import { getUsers, useUsersDispatch, useUsersState } from './UsersContext';
 
 function Users() {
   const [userId, setUserId] = useState(null);
-  const [state, refetch] = useAsync(getUsers, []);
-  const { loading, data: users, error } = state;
+  const state = useUsersState();
+  const dispatch = useUsersDispatch();
+
+  const { data: users, loading, error } = state.users;
+  const fetchData = () => {
+    getUsers(dispatch);
+  };
 
   if (loading) return <div>로딩중..</div>;
   if (error) return <div>에러가 발생했습니다</div>;
