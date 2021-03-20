046df90c9b1a7fa55237f7596e6a587833ed4135
sonchaegeon <thscorjs1002@naver.com>
2021-03-20 10:21:42 +0900

    context api with typescript

--- ts-react-tutorials/src/App.tsx
+++ ts-react-tutorials/src/App.tsx
@@ -1,8 +1,13 @@
 import React from 'react';
 import ReducerSample from './components/ReducerSample';
+import { SampleProvider } from './components/SampleContext';
 
 const App: React.FC = () => {
-  return <ReducerSample />;
+  return (
+    <SampleProvider>
+      <ReducerSample />
+    </SampleProvider>
+  );
 };
 
 export default App;
